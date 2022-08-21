2cfef32a913fb6cf0c58414ac845850840caccda
Jack Garland <34702783+JackTGarland@users.noreply.github.com>
2022-08-21 21:53:02 +0100

    added comments & readme
    Wahoo a readme! & comments, this is looking like a user friendly project at last.

---
+++ README.md
@@ -0,0 +1,36 @@
+# DHT-22-Guide
+Just a small python script that get's the humidity and temprature and stores it in a text file.
+
+This is a simple set up for the DHT-22 thermometer that can be used with the raspberry pi.
+
+I was not a fan of the guides that were out there so I set out to make my own building off the guides.
+
+There are two forms of this, the basic and advanced.
+
+The Basic will just print the information and write it to a text file that will just keep growing till it reaches max file size.
+UNLESS You use the renamer script
+
+I would recomend using crontab for this, below is what I have mine running on, with the temp checking script running every 5 minuites. 
+Then the renamer to store it in a seprate location running 2 minuites to midnight.
+
+`*/5 * * * * python3 humidity2.py
+58 23 * * * python3 rename.py`
+
+You will need to add the file path to these locations if they are not in your home derectory. You should not need sudo for these scripts eaither so just `crontab -e` will work.
+
+The advanced will write it to a SQL database and a basic API system is present should you want to make use of that. 
+
+There are some commands that need to be run first. And all asuming it is on a raspberry pi.
+
+Make sure you have Python as most of this is written in it. 
+
+`python -m pip install cursor`
+
+`python -m pip install mysql-connector-python`
+
+The advanaced is still much more in a work in progress and I am debating it's implimentation. Eaither a public facing SQL database, or some kind of rest API service written in C# that will parse the data to SQL.
+
+One is safer but harder to set up and I don't want to make this to complicated. 
+
+Want to have a service on the pi that reads the most recent entry to the temp file and sends that data to some other service, be it on the pi or on a seprate server.
+
--- temp checker basic.py
+++ temp checker basic.py
@@ -1,23 +1,22 @@
-import Adafruit_DHT
-from datetime import datetime
-from time import sleep
+import Adafruit_DHT #This is the core, it takes data from the sensor.
+from datetime import datetime #Used to get date time
+from time import sleep # used to be able to sleep. I noticed my sensor would need two checks to get the right reading.
 
 DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+DHT_PIN = 4 # Being honest, no idea. But I assume it is the pin it's reading the data from on the pi.
 humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 sleep(1)
 
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN) # getting the second accurate reading. Can add a print line if you don't belive me.
 
-humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-if humidity is not None and temperature is not None:
+if humidity is not None and temperature is not None: # Lets make sure we got some data. 
         date_time = datetime.now()
-        ts = date_time.strftime("%d-%m-%Y, %H:%M:%S")
-        writedata = (ts + "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        ts = date_time.strftime("%d-%m-%Y, %H:%M:%S")#Setting the string format for date time.
+        writedata = (ts + "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity)) # Assinging all the data to a single varible.
         print(writedata)
-        f = open("temp.txt", "w")
-        f.write("\n")
-        f.write(writedata)
+        f = open("temp.txt", "w") #The file we are writing to, and Wright permissions. It will make that file if not exsisting.
+        f.write("\n") #Creates a new line so the data looks nice.
+        f.write(writedata) #The write funtion can only take one argument so we had to reduce it down ealier. See there was a reason
 else:
         print("Failed to retrieve data from humidity sensor")
 
